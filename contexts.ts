8a850a16816dd1eb181c516eb9fea8c2b19d1934
Ashi Krishnan <queerviolet@github.com>
2019-03-23 13:40:28 +0800

    Better name for the clock context (tho less cute). A classier inspector.

--- contexts.ts
+++ contexts.ts
@@ -1,6 +1,6 @@
 export const GLContext = 'GL.Context'
 export const DataContext = 'Data.Client'
-export const Clock = 'tick, tock goes the clock'
+export const Clock = 'Clock.now'
 
 export const Camera = {
   uProjection: 'Camera.uProjection'
