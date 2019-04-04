264e6daf7590c4b95199570698d786015c72235a
Ashi Krishnan <queerviolet@github.com>
2019-04-04 22:01:41 +0800

    Panels animating and stage positioned.

--- panel.tsx
+++ panel.tsx
@@ -2,12 +2,29 @@ import * as React from 'react'
 import { basename } from 'path'
 import useSource from './use-source'
 
-export function Panel({ file }) {
+import { StageCoordinates, StageRect, pixelRect } from './stage'
+
+
+type Props = {
+  file: string
+  frame?: StageRect
+}
+
+export function Panel({ file, frame=[[-5, -5], [5, 5]] }: Props) {
   const name = basename(file)
   const src = useSource(file)
 
-  return <div className={`panel ${src ? 'loaded' : ''}`}>
+  return <div className={`panel ${src ? 'loaded' : ''}`} style={styleFromFrame(frame)}>
     <h1>{name}</h1>    
     <code>{src}</code>
   </div>
+}
+
+const styleFromFrame = (frame: StageRect) => {
+  const style = {}
+  const px = pixelRect(frame)
+  for (const [key, value] of Object.entries(px)) {
+    style[`--panel-${key}`] = value + 'px'
+  }
+  return style
 }
--- stage.ts
+++ stage.ts
@@ -2,15 +2,22 @@ import { applyLetterbox, Box } from './letterbox'
 
 const WIDTH = 16
 const HEIGHT = 9
+const X_MIN = -WIDTH
+const Y_MIN = -HEIGHT
+const X_MAX = WIDTH
+const Y_MAX = HEIGHT
+
+const MIN = [X_MIN, Y_MIN]
+const MAX = [X_MAX, Y_MAX]
 
 let frame: Box | null = null
 applyLetterbox(WIDTH / HEIGHT, box => frame = box)
 
 export const STAGE_QUAD = [
-  WIDTH, HEIGHT, 0,
-  -WIDTH, HEIGHT, 0,
-  WIDTH, -HEIGHT, 0,
-  -WIDTH, -HEIGHT, 0
+  X_MAX, Y_MAX, 0,
+  X_MIN, Y_MAX, 0,
+  X_MAX, Y_MIN, 0,
+  X_MIN, Y_MIN, 0
 ]
 
 export const frameCoordsFrom = ({
@@ -20,3 +27,33 @@ export const frameCoordsFrom = ({
     2 * WIDTH * (clientX - frame.left) / frame.width - WIDTH,
     2 * HEIGHT * (clientY - frame.top) / frame.height - HEIGHT,
   ]
+
+export type StageCoordinates = [number, number]
+
+export function toPosition([x, y]: StageCoordinates) {
+  const tx = (x - X_MIN) / (2 * WIDTH)
+  const ty = (y - Y_MIN) / (2 * HEIGHT)
+  return {
+    tx, ty,
+    top: frame.top + frame.height * ty,
+    left: frame.left + frame.width * tx,
+    bottom: frame.bottom + frame.height * (1 - ty),
+    right: frame.right + frame.width * (1 - tx),
+  }
+}
+
+export type StageRect = [StageCoordinates, StageCoordinates]
+
+export function pixelRect([p0, p1]: StageRect) {
+  const pos0 = toPosition(p0)
+  const pos1 = toPosition(p1)
+  const { top, left } = pos0
+  const { bottom, right } = pos1
+  const width = pos1.left - left
+  const height = pos1.top - top
+  return {
+    top, bottom, left, right, width, height
+  }
+}
+
+export function vbox() {}
